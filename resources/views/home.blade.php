8a0030e27c071369626a3fea4bb7965d5fa23609
Piotrszef <35202647+Piotrszef@users.noreply.github.com>
2021-11-30 14:06:03 +0100

    Update home.blade.php
    Realizacje widzą tylko osoby które są do niej przypisane.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 
-                <div class="panel-heading" style="font-size:30px;">Twoje realizacje
+                <div class="panel-heading" style="font-size:30px;">Twoje realizacje - {{Auth::user()->role}}
                 @if( Auth::user()->role == 'superadmin' || Auth::user()->role == 'kierownik_projektu')
                     <div style="float:right;"><a href="/homecreate">&#10010;</a></div>
                 @endif
@@ -18,14 +18,17 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @foreach($memberList as $member)
-                    @endforeach
-                    @foreach ($realizationList as $realization) 
-                        @if($realization->id_realization == $member->id_realization)
-                        <div style=" float:left;padding:5px; text-align: center;"><a href="/asset/{{ $realization->id_realization }}"><div style="font-size:50px;">&#128448;</div></a>{{ $realization->name }}</div>
-                        @endif
-                    @endforeach
                     
+                    @foreach($memberList as $member)
+                        @foreach ($realizationList as $realization) 
+                            @if($realization->id_realization == $member->id_realization || Auth::user()->role == 'superadmin')
+                                <div style=" float:left;padding:5px; text-align: center;"><a href="/asset/{{ $realization->id_realization }}"><div style="font-size:50px;">&#128448;</div></a>{{ $realization->name }}</div>
+                            @endif
+                            
+                        @endforeach   
+                        
+                    @endforeach
+   
                 </div>
             </div>
         </div>
