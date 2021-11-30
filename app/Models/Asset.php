178aee8002ff1fd6c3621169531c7ea0d6685ec1
Piotrszef <35202647+Piotrszef@users.noreply.github.com>
2021-11-30 10:18:22 +0100

    Create Asset.php

---
+++ app/Models/Asset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asset extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_realization', 'name', 'urn',
+    ];
+
+    
+}
